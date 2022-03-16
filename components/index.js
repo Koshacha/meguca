15df6cb6d777383d2d8bc63f94b8cff4b9c1348f
Ilya M <baka@derpy.ru>
2022-03-17 01:32:16 +0700

    tailwind css init

---
+++ components/index.js
@@ -0,0 +1,5 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Lead from "./Lead";
+
+export { Header, Footer, Lead };
---
+++ pages/_document.jsx
@@ -0,0 +1,29 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            type="text/css"
+            href="https://fonts.googleapis.com/css?family=Nunito"
+            media="all"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
