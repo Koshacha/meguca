3ddd6238b274e76fa9c475d3bcbcfa9dcd01c8c8
Ilya M <baka@derpy.ru>
2022-03-17 02:33:04 +0700

    Header init

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,49 +1,39 @@
+import Link from "next/link";
+import LampSvg from "../svg/lamp.svg";
+
 export default function Header() {
   return (
     <header className="relative w-full px-8 text-gray-700 bg-white body-font">
       <div className="container flex flex-col flex-wrap items-center justify-between py-5 mx-auto md:flex-row max-w-3xl">
-        <a
-          href="#_"
-          className="relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-black select-none"
-        >
-          tails.
-        </a>
+        <Link href="/">
+          <a className="relative z-10 flex items-center w-auto text-2xl font-extrabold leading-none text-black select-none">
+            meguca.
+          </a>
+        </Link>
 
         <nav className="top-0 left-0 z-0 flex items-center justify-center w-full h-full py-5 -ml-0 space-x-5 text-base md:-ml-5 md:py-0 md:absolute">
-          <a
-            href="#_"
-            className="relative font-medium leading-6 text-gray-600 hover:text-gray-900"
-          >
-            <span className="block">Home</span>
-          </a>
-          <a
-            href="#_"
-            className="relative font-medium leading-6 text-gray-600 hover:text-gray-900"
-          >
-            <span className="block">Features</span>
-          </a>
-          <a
-            href="#_"
-            className="relative font-medium leading-6 text-gray-600 hover:text-gray-900"
-          >
-            <span className="block">Pricing</span>
-          </a>
-          <a
-            href="#_"
-            className="relative font-medium leading-6 text-gray-600 hover:text-gray-900"
-          >
-            <span className="block">Blog</span>
-          </a>
+          <Link href="/posts">
+            <a className="relative font-medium leading-6 text-gray-600 hover:text-gray-900">
+              <span className="block">Posts</span>
+            </a>
+          </Link>
+          <Link href="/projects">
+            <a className="relative font-medium leading-6 text-gray-600 hover:text-gray-900">
+              <span className="block">Showcase</span>
+            </a>
+          </Link>
+          <Link href="/contact">
+            <a className="relative font-medium leading-6 text-gray-600 hover:text-gray-900">
+              <span className="block">Contacts</span>
+            </a>
+          </Link>
         </nav>
 
         <div className="relative z-10 inline-flex items-center space-x-3 md:ml-5 lg:justify-end">
           <span className="inline-flex rounded-md shadow-sm">
-            <a
-              href="#"
-              className="inline-flex items-center justify-center px-4 py-2 text-base font-medium leading-6 text-white whitespace-no-wrap bg-blue-600 border border-blue-700 rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              Sign up
-            </a>
+            <button className="w-9 h-9 bg-gray-200 rounded-lg dark:bg-gray-600 flex items-center justify-center hover:ring-2 ring-gray-300 transition-all">
+              <LampSvg className="w-5 h-5 text-gray-800 dark:text-gray-200" />
+            </button>
           </span>
         </div>
       </div>
