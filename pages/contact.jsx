8cc02684a8ff6a25ca1819e3de8187322fc9afb2
Ilya M <baka@derpy.ru>
2022-03-18 01:12:36 +0700

    empty page

---
+++ pages/contact.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Header, Footer, Lead, Frameworks } from "../components";
+
+export default function ContactPage() {
+  return (
+    <>
+      <Header />
+      <Lead />
+      <Frameworks />
+      <Footer />
+    </>
+  );
+}
