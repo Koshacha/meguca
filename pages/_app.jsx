8f415255d0c6861294d0ca2dbdea3981963f38bc
Ilya M <baka@derpy.ru>
2022-03-18 16:41:00 +0700

    add GTM

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,8 +1,22 @@
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZEGKCG1L3R"
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="googletagmanager">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZEGKCG1L3R');
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
