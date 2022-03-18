f032a4705aa02f527e258f719c99f7719e82faaa
Ilya M <baka@derpy.ru>
2022-03-18 16:41:14 +0700

    add temp title

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,6 +5,9 @@ import { Header, Footer, Lead, Frameworks } from "../components";
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Ilya Mazunin — JavaScript Developer</title>
+      </Head>
       <Header />
       <Lead />
       <Frameworks />
